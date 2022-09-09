c3bcc05f9c4bdc321c90ec8b62b574eceaa4a239
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-08 20:20:38 -0700

    Using puts to print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/** 
- * Main - Print a string using puts followed
- *	  by a new line
+/**
+ * main - starts the main function
+ *
+ * Description: Using puts to print a string
  *
  * Return: Always 0
  */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-		return (0);
+	return (0);
 }
