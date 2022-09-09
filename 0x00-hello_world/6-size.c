1af34c98835b90aead86b4de0143ead4892211ea
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-08 21:13:05 -0700

    Printing size of various types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - start the main function
+ *
+ * Description: Using multiple printf fuction to print different types
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %li byte(s)\n", sizeof(char));
+	printf("Size of an int: %li byte(s)\n", sizeof(int));
+	printf("Size of a long int: %li byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %li byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+
+	return (0);
+}
