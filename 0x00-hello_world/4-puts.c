b7590b011123da4ed58a9a04bbddf9ec189e63bd
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-08 19:36:12 -0700

    using the printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /** 
- * Main- Print a string using puts followed
- *	 by a new line
+ * Main - Print a string using puts followed
+ *	  by a new line
  *
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+		return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: Prints a string of characters using printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
