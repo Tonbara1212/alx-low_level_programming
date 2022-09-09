2ae1e964c62c020f6b4f507e2a304366afe4d427
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-09 23:34:26 +0100

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+/**
+ * main - starts the main function
+ *
+ * Description: Prints 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	
+	return (0);
+}
