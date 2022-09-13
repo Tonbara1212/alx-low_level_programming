fb9caa75fc3484c44aae04e20e5cc40764398097
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-13 13:44:37 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * main - starts the main function
+ *
+ * Description: Print _putchar using _putchar
+ *
+ * Return: Always 1 on success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0: A c program that prints _putchar folloed by a new line
