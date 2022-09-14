660b807e05bf58de3690b0a5cf9633167e2092fc
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-14 03:02:16 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,23 @@
-#include "main.h" 
-  
- /** 
-  * main - This program prints the phrase Hello, World 
-  * Return: Always 0 (Success) 
-  */ 
- int main(void) 
- { 
-         char ch[] = "_putchar"; 
-         int i; 
-  
-         for (i = 0; ch[i] != '\0'; i++) 
-         { 
-                 _putchar(ch[i]); 
-         } 
-  
-         _putchar('\n'); 
-         return (0); 
- }
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
