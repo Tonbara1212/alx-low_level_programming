77746ba551a90de778498cc7820a60e49a6455b0
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-14 04:36:04 -0700

    check for alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabets
+ *
+ * Return: Return 1 if character is an alphabet; else return 0
+ */
+int _isalpha(int c)
+{
+
+		if ((c >= 'a' && c <= 'z') ||
+		    (c >= 'A' && c <= 'Z')
+
+
+			return (1);
+
+		else
+
+			return(0);
+
+}
