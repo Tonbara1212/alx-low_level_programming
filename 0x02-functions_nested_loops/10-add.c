46b394174a9b84d14c3f30190afb099f9fa82ad3
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-14 15:51:35 -0700

    Sum of operands

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h" 
+  
+ /** 
+  * add - Adds two inyegers to return result 
+  * @w: Operand 1
+  * @x: Operand 2 
+  * 
+  * Return: Sum of operands 
+  */ 
+ int add(int w, int x) 
+ { 
+         int sum; 
+  
+         sum = w + x; 
+  
+         return (sum); 
+ }
