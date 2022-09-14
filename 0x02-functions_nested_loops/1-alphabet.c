9b86d2b1dc60753f5e4519eed960c8cbd3732c59
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-14 03:48:08 -0700

    print lowercase a-z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print lower case a-z
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z', c++)
+
+		_putchar('c');
+
+	_putchar('\n');
+
+	return (0);
+}
