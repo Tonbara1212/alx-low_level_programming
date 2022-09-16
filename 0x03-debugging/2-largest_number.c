9c3a6c5b8de67ef1492320cf881b15849dfc755a
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-15 21:41:01 -0700

    Largest of three numbers

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * largest_number - checks for largest of 3 numbers
+ * @x: first numner
+ * @y: second number
+ * @z: third number
+ * Return: returns largest numbe
+ */
+
+int largest_number(int x, int y, int z)
+{
+	int largest;
+	if (x >= y && x >= z)
+	{
+		largest = x;
+	}
+	else if (y >= x && y >=z)
+	{
+		largest = y;
+	}
+	else
+	{
+		largest = z;
+	}
+	return (largest)
+}
