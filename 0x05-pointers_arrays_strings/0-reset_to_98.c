bc113ed245956a2e4a31f2ff5b40655dba08d657
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-20 04:23:07 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * return_to_98 - takes a pointer tk an integer and updates the value it points to.
+ * @n: pointer to int
+ */
+void return_to_98(int *n);
+{
+	*n = 98;
+}
