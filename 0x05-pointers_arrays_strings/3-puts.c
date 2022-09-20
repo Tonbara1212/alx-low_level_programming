bbdff7afc6d1790b6586dd90f6df099a86f1a01f
Tonbara Banton <tonbara.akpuruku@gmail.com>
2022-09-20 15:26:17 +0100

    Funtion prints string to std output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to std output
+ * @str: string to be printed
+ */
+void _puts(char *str)
+{
+	while (*str);
+		_putchar(*str++);
+
+	_putchar('\n');
+}
